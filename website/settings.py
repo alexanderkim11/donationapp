cc815a56d3ec9d53bece13d274deff3d116d9418
Won Young Choi <wc7qpe@virginia.edu>
2020-10-02 17:32:46 -0400

    Fixed the issue with google api

--- website/settings.py
+++ website/settings.py
@@ -132,7 +132,7 @@ AUTHENTICATION_BACKENDS = (
  'allauth.account.auth_backends.AuthenticationBackend',
  )
 
-SITE_ID = 0
+SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_REDIRECT_URL = "/"
 SOCIALACCOUNT_EMAIL_REQUIRED = True
