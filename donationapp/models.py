5790a81c8c6c4db1ae3aa24a651c976fb0c6edc5
Won Young Choi <wc7qpe@virginia.edu>
2020-10-15 15:42:21 -0400

    Causes model can have a total money set to 0 now

--- donationapp/models.py
+++ donationapp/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator
 class Cause(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=512)
-    total_money = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0.01)])
+    total_money = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0.00)])
     goal = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0.01)])
 
     def __str__(self):
