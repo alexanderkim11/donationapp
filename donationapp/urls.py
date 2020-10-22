3f9faa7293e3869b3e94719f902b478cf435ba20
eal172 <eal8hs@virginia.edu>
2020-10-22 17:08:16 -0400

    Created volunteer model, volunteer page and create volunteer form

--- donationapp/urls.py
+++ donationapp/urls.py
@@ -6,5 +6,7 @@ app_name="donationapp"
 urlpatterns = [
     path('', views.index, name="index"),
     path('account/', views.account, name="account"),
-    path('causes/', views.causes, name="causes")
+    path('causes/', views.causes, name="causes"),
+    path('volunteering/', views.volunteer_opportunities, name="volunteering"),
+    path('create/', views.create_opportunity, name="create")
 ]
--- donationapp/views.py
+++ donationapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Cause, Transaction
-from .forms import TransactionForm
+from .models import Cause, Transaction,Volunteer_Opportunity
+from .forms import TransactionForm, VolunteerForm
 
 # Create your views here.
 def index(request):
@@ -13,7 +13,7 @@ def index(request):
         if form.is_valid():
             form.save()
 
-    context = {'form': form}
+    context = {'form': form,'nbar': 'home'}
     return render(request, "donationapp/index.html", context)
 
 def account(request):
@@ -23,3 +23,17 @@ def causes(request):
     latest_cause_list = Cause.objects.all()
     context = {'latest_cause_list': latest_cause_list,'nbar': 'causes'}
     return render(request, 'donationapp/causes.html',context)
+
+def volunteer_opportunities(request):
+    latest_volunteer_list = Volunteer_Opportunity.objects.all()
+    context = {'latest_volunteer_list': latest_volunteer_list,'nbar': 'volunteer'}
+    return render(request, 'donationapp/volunteer_opportunities.html',context)
+
+def create_opportunity(request):
+    form = VolunteerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {'form': form}
+
+    return render(request, 'donationapp/create_volunteer', context)
