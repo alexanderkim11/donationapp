664c454aa7fd93320701ab5af223ac0bfe4e0d73
Won Young Choi <wc7qpe@virginia.edu>
2020-10-23 14:38:56 -0400

    Set up basic Paypal API

--- donationapp/urls.py
+++ donationapp/urls.py
@@ -6,5 +6,6 @@ app_name="donationapp"
 urlpatterns = [
     path('', views.index, name="index"),
     path('account/', views.account, name="account"),
-    path('causes/', views.causes, name="causes")
+    path('causes/', views.causes, name="causes"),
+    path('checkout/<int:pk>/', views.checkout, name='checkout'),
 ]
