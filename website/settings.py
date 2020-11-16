3cf6997b4f7f64e4e8e21564dba4f5550706b47d
WonyoungChoi-0 <62449866+WonyoungChoi-0@users.noreply.github.com>
2020-11-15 23:43:29 -0500

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -103,8 +103,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES = {
      'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),	        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
         'NAME': 'd1e5j5k19bda4o',
 
